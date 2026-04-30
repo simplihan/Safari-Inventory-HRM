2ec8cd8bbc57b1e0ebd34b201a5fa9f2e6acb1e9
Harendra Kumar <harendra@simplihan.com>
2026-04-30 15:30:25 +0300

    Update Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useAuth } from '../contexts/AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
