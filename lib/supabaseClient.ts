f79909eac20a20f6c32c21efdc78f1df2646844f
Harendra Kumar <harendra@simplihan.com>
2026-04-30 17:02:56 +0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.https://hustixdjpwuqmpzspkvn.supabase.co
 const supabaseAnonKey = process.env.sb_publishable_CnnasDBv9eL-_g6Jayf2bw_NMFuDDoq
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
