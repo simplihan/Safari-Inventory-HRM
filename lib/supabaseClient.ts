6111d34d3872956237e4db91a791cb662b1a6ea0
Harendra Kumar <harendra@simplihan.com>
2026-04-30 16:01:47 +0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = process.env.https://hustixdjpwuqmpzspkvn.supabase.co
+const supabaseAnonKey = process.env.sb_publishable_CnnasDBv9eL-_g6Jayf2bw_NMFuDDoq
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
